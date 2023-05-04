17140daf9988699623b143f2d5761ce71d91f29f
aayush20008 <89084060+aayush20008@users.noreply.github.com>
2023-05-05 04:11:11 +0530

    calendar added

---
+++ EventHUB-PK_Work/app/src/main/java/com/example/eventhub/ReminderReceiver.java
@@ -0,0 +1,41 @@
+package com.example.eventhub;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import static android.content.Context.NOTIFICATION_SERVICE;
+import androidx.core.app.NotificationCompat;
+
+import java.io.File;
+
+public class ReminderReceiver extends BroadcastReceiver {
+    private static final String CHANNEL_ID = "REMINDER_CHANNEL";
+    private static final int NOTIFICATION_ID = 123;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Get the reminder message from the intent
+        String message = intent.getStringExtra("message");
+
+        // Create a notification and show it
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Reminder Notifications", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.blyd) //change this
+                .setContentTitle("EventsHUB")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+}
